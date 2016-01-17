3832d15edbda3200175867519e407c364ff4353b
Ernest Chan <Ernest Chan>
2016-01-17 07:29:16 -0600

    added in virtual env

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ernes\Documents\Visual Studio 2015\Projects\pickturethis\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
